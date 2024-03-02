6dccef18abe302f3e5aee186b4c973586d46d924
Kevin Dev <kevin.developer71@gmail.com>
2024-03-01 21:27:30 -0600

    log

--- src/actions/getUsers.ts
+++ src/actions/getUsers.ts
@@ -6,6 +6,8 @@ export const getUsers = async (offset: number, limit: number) => {
     const url = `https://api.slingacademy.com/v1/sample-data/users?offset=${offset}&limit=${limit}`
     const response = await fetch(url)
     const data = (await response.json()) as UserAPIResponse
+    console.log("=====================")
+    console.log(data)
     return data.users
   } catch (error: unknown) {
     console.log(error)
