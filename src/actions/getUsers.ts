6b8893feb9b56a8b032fbbff378769e55f418e93
Kevin Dev <kevin.developer71@gmail.com>
2024-03-01 21:35:09 -0600

    fix: remove use server label

--- src/actions/getUsers.ts
+++ src/actions/getUsers.ts
@@ -1,4 +1,3 @@
-'use server'
 import { UserAPIResponse } from '@/types/user'
 
 export const getUsers = async (offset: number, limit: number) => {
@@ -6,8 +5,6 @@ export const getUsers = async (offset: number, limit: number) => {
     const url = `https://api.slingacademy.com/v1/sample-data/users?offset=${offset}&limit=${limit}`
     const response = await fetch(url)
     const data = (await response.json()) as UserAPIResponse
-    console.log("=====================")
-    console.log(data)
     return data.users
   } catch (error: unknown) {
     console.log(error)
